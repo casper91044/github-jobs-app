d867019ecf000d7df495a1a67ff031bc2afb92a7
casper91044 <casper91044@mail.ru>
2020-10-20 10:32:57 +0300

    Updates

---
+++ precache-manifest.00be7f15e05b65938ac2bfca2c81fee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a09428fa72bb8ef61b40b2271976db2",
+    "url": "/github-jobs-app/index.html"
+  },
+  {
+    "revision": "5973b692b09877ef51a0",
+    "url": "/github-jobs-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c7f8cfdb0ee0c87c934",
+    "url": "/github-jobs-app/static/js/2.1b43c633.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs-app/static/js/2.1b43c633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5973b692b09877ef51a0",
+    "url": "/github-jobs-app/static/js/main.0c0fd8e1.chunk.js"
+  },
+  {
+    "revision": "8b73d2fe66d00593cdb4",
+    "url": "/github-jobs-app/static/js/runtime-main.1656721a.js"
+  }
+]);
